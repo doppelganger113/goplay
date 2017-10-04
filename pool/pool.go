c4789d5831fc2c24b173acd5f2e0137fffe99043
marko <doppelganger113>
2017-10-04 23:44:02 +0200

    + Added Runner main and Pool

---
+++ pool/pool.go
@@ -0,0 +1,80 @@
+package pool
+
+import (
+	"sync"
+	"io"
+	"errors"
+	"log"
+)
+
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ErrPoolClosed = errors.New("pool has been closed")
+
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("size value to small")
+	}
+
+	return &Pool{
+		factory:   fn,
+		resources: make(chan io.Closer, size),
+	}, nil
+}
+
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		log.Println("Acquire:", "Shared resource")
+
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+
+		return r, nil
+
+	default:
+		log.Println("Acquired:", "New resource")
+		return p.factory()
+	}
+}
+
+func (p *Pool) Release(r io.Closer) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+	default:
+		log.Println("Release:", "In Queue")
+		r.Close()
+	}
+}
+
+func (p *Pool) Close() {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		return
+	}
+
+	p.closed = true
+
+	close(p.resources)
+
+	for r := range p.resources {
+		r.Close()
+	}
+}
